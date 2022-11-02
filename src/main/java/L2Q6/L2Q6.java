f73b8f6148d76b9c810c09baa75990706af48117
PoisonDarterz <jc.chuan.0303@gmail.com>
2022-11-02 15:41:19 +0800

    errata l2q6

--- src/main/java/L2Q6/L2Q6.java
+++ src/main/java/L2Q6/L2Q6.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 class L2Q6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the amount of water in kg: ");
+        System.out.print("Enter the amount of water in grams: ");
         double M = sc.nextDouble();
-        System.out.print("Enter the initial temperature in Celsius: ");
-        double I = sc.nextDouble();
-        System.out.print("Enter the final temperature in Celsius: ");
-        double F = sc.nextDouble();
-        double Q = M * (F - I) * 4184;
+        System.out.print("Enter the initial temperature in Fahrenheit: ");
+        double FI = sc.nextDouble();
+        System.out.print("Enter the final temperature in Fahrenheit: ");
+        double FF = sc.nextDouble();
+        double I = (FI - 32) * 5 / 9;
+        double F = (FF - 32) * 5 / 9;
+        double Q = M/1000 * (F - I) * 4184;
         System.out.printf("The energy needed is %.1f", Q);
     }
 }
